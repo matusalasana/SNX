c2e296158145bb0fefbd4e28d87e0ef263e1362f
Sana Matusala <matusalasana@gmail.com>
2026-05-24 11:24:13 +0300

    connect to mongodb atlas

---
+++ server/src/config/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { MONGODB_URI } from "./env";
+
+export const connectMongoDB = async() => {
+  try{
+    await mongoose.connect(MONGODB_URI)
+    console.log("✅ Database Connected Successfully");
+  }catch(err){
+    console.log("❌ Database connection error:", err);
+  }
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
-import { app } from "./app"
-
+import { app } from "./app";
+import { connectMongoDB } from "./config/db";
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
+  connectMongoDB();
 })
