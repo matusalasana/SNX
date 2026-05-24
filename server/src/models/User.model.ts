f0709254d11c526008c0510af0af24eeb5738324
Sana Matusala <matusalasana@gmail.com>
2026-05-24 13:42:47 +0300

    feat: add refresh token in user model to store it in db

--- server/src/models/User.model.ts
+++ server/src/models/User.model.ts
@@ -4,6 +4,7 @@ export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  refreshToken?: string;
   role: "admin";
 }
 
@@ -11,7 +12,8 @@ const userSchema = new Schema<IUser>(
   {
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true, minlength: 6 },
+    password: { type: String, required: true, minlength: 8 },
+    refreshToken: { type: String },
     role: { type: String, default: "admin" },
   },
   { timestamps: true }
