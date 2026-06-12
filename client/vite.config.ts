f404c0cb945912c3ee498221df77ad7e1991d0cc
Sana Matusala <matusalasana@gmail.com>
2026-06-12 17:44:00 +0300

    refactor: fix npm install, remove old version tailwindcss

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(() => {
-  return {
-    plugins: [tailwindcss(), react()], 
-  };
-});
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
