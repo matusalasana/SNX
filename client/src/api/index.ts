9fb50296397735e428ace9d9e2527f67cb70f536
Sana Matusala <matusalasana@gmail.com>
2026-06-18 01:25:47 +0300

    feat: add deployed api in the client side, fix bugs

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
-  baseURL: 'http://localhost:9000/api/v1',
+  baseURL: VITE_API_URL,
   withCredentials: true
 });
 
