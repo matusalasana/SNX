42a28911f96bb6b13304d0da8201fa7049dbe0f9
Sana Matusala <matusalasana@gmail.com>
2026-06-17 19:25:15 +0300

    fix(deployement): add deployed link to allowed origins in cors settings

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,22 +9,12 @@ export const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   CLIENT_ORIGIN,
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-};
+]
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: CLIENT_ORIGIN,
+  credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 
