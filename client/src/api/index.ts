e258b80d3657b2debdccc6b2baa19a43b2985c28
Sana Matusala <matusalasana@gmail.com>
2026-06-18 14:31:39 +0300

    feat(chore): add rewrite proxy setting in vercel.json

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
-const VITE_API_URL = import.meta.env.VITE_API_URL;
+
+const getBaseURL = () => {
+  if (import.meta.env.DEV) {
+    return "http://localhost:3000/api/v1";
+  }
+  return "/api/v1";
+};
+
 const api = axios.create({
-  baseURL: VITE_API_URL,
+  baseURL: getBaseURL(),
   withCredentials: true
 });
 
