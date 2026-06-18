c02ac7f3a0a427354f8506b1f5d0ae084ce2e869
Sana Matusala <matusalasana@gmail.com>
2026-06-18 19:57:43 +0300

    feat: implement scroll to top on page change

--- client/src/layouts/AdminLayout.tsx
+++ client/src/layouts/AdminLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/admin/Navbar";
 import Footer from "../components/common/Footer";
+import ScrollToTop from "../components/common/ScrollToTop";
 
 export default function AdminLayout() {
   return (
@@ -8,6 +9,7 @@ export default function AdminLayout() {
       <Navbar />
 
       <main className="flex-1">
+        <ScrollToTop />
         <Outlet />
       </main>
 
--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
+import ScrollToTop from "../components/common/ScrollToTop";
 
 export default function Layout() {
   return (
@@ -8,6 +9,7 @@ export default function Layout() {
       <Navbar />
 
       <main className="flex-1">
+      <ScrollToTop />
         <Outlet />
       </main>
 
