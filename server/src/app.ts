c6818e2681e1e264b020b718cc4c72bd5adce64f
Sana Matusala <matusalasana@gmail.com>
2026-06-18 14:45:42 +0300

    fix: allow cors to accept any origins for testing

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,27 +1,11 @@
 import express from "express";
 import routes from "./routes/index";
 import cors from "cors";
-import { CLIENT_ORIGIN } from "./configs/env"
-
 
 export const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  CLIENT_ORIGIN,
-];
-
 app.use(cors({
-  origin: (origin, callback) => {
-    // allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
-    return callback(new Error("Not allowed by CORS"));
-  },
+  origin: '*',
   credentials: true,
 }));
 app.use(express.json());
